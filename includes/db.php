0b3c18cf48dd72e5dae97a533de782844c2c4602
Rtannu <info.raj.mnit@gmail.com>
2018-07-08 23:32:23 +0530

    This folder contains all database connectivity file

---
+++ includes/db.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("localhost","root","","project");
+if(mysqli_connect_errno())
+      {
+         echo"Faild to connect to MySql".mysqli_connect_error();
+      }
+ ?>
---
+++ includes/foter.php
@@ -0,0 +1,3 @@
+<div id="foter"> 
+                 <h2 style="text-align:center; color:pink; padding-top:30px;">&copy;2016 by ROUSHAN_RAJ&SHUBHAM_SAGAR</h2>
+ </div>

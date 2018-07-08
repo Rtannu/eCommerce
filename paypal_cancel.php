f723b53d7e5935917793b3e25f76282db0370858
Rtannu <info.raj.mnit@gmail.com>
2018-07-09 00:22:03 +0530

    contain logic of paypal cancel

---
+++ paypal_cancel.php
@@ -0,0 +1,11 @@
+
+<html>
+     <head>
+           <title>Payment cancel</title>
+     </head>
+     <body>
+         <h2>Payment was cancelled!</h2>
+         <h3>your payment was  not successful,go to shop and try again</h3>
+         <a  href="http://http://www.online_book.com/mystore/">Go to Back</a>
+     </body>
+</html>
